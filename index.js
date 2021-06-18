135d27bbaac146738efd867dcfde941b375027f1
hamzapatel <57700298+hamzapatel@users.noreply.github.com>
2021-06-18 20:18:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+var http = require("http");
+var ejs = require("ejs");
+var fs = require("fs");
+
+var server = http.createServer(function (request, response) {
+  if (request.url === "/Monday" && request.method === "GET") {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    var htmlContent = fs.readFileSync("./views/monday.ejs", "utf8");
+    response.end(
+      ejs.render(htmlContent, {
+        filename: "monday.ejs",
+        timeStamp: getWeekDayDate(1),
+      })
+    );
+  } else if (request.url === "/Tuesday" && request.method === "GET") {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    var htmlContent = fs.readFileSync("./views/tuesday.ejs", "utf8");
+    response.end(
+      ejs.render(htmlContent, {
+        filename: "tuesday.ejs",
+        timeStamp: getWeekDayDate(2),
+      })
+    );
+  } else if (request.url === "/Wednesday" && request.method === "GET") {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    var htmlContent = fs.readFileSync("./views/wednesday.ejs", "utf8");
+    response.end(
+      ejs.render(htmlContent, {
+        filename: "wednesday.ejs",
+        timeStamp: getWeekDayDate(3),
+      })
+    );
+  } else {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    var htmlContent = fs.readFileSync("./views/index.ejs", "utf8");
+    response.end(
+      ejs.render(htmlContent, {
+        filename: "wednesday.ejs",
+        timeStamp: getWeekDayDate(1),
+      })
+    );
+   }
+});
+
+const getWeekDayDate = (weekDay) => {
+  let currentDate = new Date();
+  currentDate.setDate(currentDate.getDate() + weekDay - currentDate.getDay());
+  return currentDate.toLocaleString();
+};
+server.listen(3000, "127.0.0.1");
---
+++ next.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var app = express();
+const port = 3000;
+
+const setTimeMiddleWare = function (req, res, next) {
+  req.currentDate = new Date();
+  next();
+};
+
+const redirectToDetails = function (req, res, next) {
+  res.redirect("details/" + req.currentDate);
+};
+
+app.use("/details/:currentDate", function (req, res, next) {
+  res.send(`Hello World details ${req.params.currentDate}`);
+});
+
+app.get(
+  "/home",
+  setTimeMiddleWare,
+  redirectToDetails,
+  function (req, res, next) {
+    console.log(req.currentDate);
+    res.send("Hello World");
+  }
+);
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
